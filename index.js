5f5dfbe1725a1b9f0b5c94f5267bcd79b3de45af
Brenda Ferrufino <ferrufinob@gmail.com>
2021-02-17 15:11:49 -0500

    commit tree 2741d041c81507b6f12f756f74af8dd0fde59aff

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+// const events = "surprise";
+function writeCards(gifter, events) {
+  let arr = [];
+  for (let i = 0; i < gifter.length; i++) {
+    arr.push(`Thank you, ${gifter[i]}, for the wonderful ${events} gift!`);
+  }
+  return arr;
+}
+writeCards(["Ada", "Brendan", "Ali"], "Bday");
+
+function countDown(number) {
+  while (number > 0) {
+    console.log(number);
+    number -= 1;
+  }
+  console.log(number);
+}
+
+countDown(10);
